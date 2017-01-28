ef281f3d0922c1fcc93644ce913bab8b38971951
07aj <developer.abhishek77@gmail.com>
2017-01-28 05:54:06 +0530

    localhost

---
+++ Dev_abhishek/app/src/main/java/com/num/aj07/dev_abhishek/app/AppConfig.java
@@ -0,0 +1,12 @@
+package com.num.aj07.dev_abhishek.app;
+
+/**
+ * Created by aj07 on 28/1/17.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.1.2/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.1.2/register.php";
+}
